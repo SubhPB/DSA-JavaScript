f8755817ab0a8f4436ac355c69c55067e4ec2e1e
SubhPB <harvirbuttar2917@gmail.com>
2024-01-09 13:45:09 -0800

    updated some functionalities of Matrix and it's tools...

--- Matrix/Matrix.js
+++ Matrix/Matrix.js
@@ -1,20 +1,61 @@
 /* -- Byimaan -- */
 
-import { verifier, check } from "./Tools";
+const { matrixTools } = require('./Tools.js');
 
 class Matrix{
 
     constructor (m,n){
-
-        // if (check('integer',m) && check('integer',n))
-
+        matrixTools.isValidMatrix(m,n)
         this.m = m;
         this.n = n;
-        this.matrix = Array.from( { length: this.m }, () => Array(this.n).fill())
+        this.matrix = Array.from( { length: this.m }, () => Array(this.n).fill());
+    };
+
+    indexErr(){
+        var type='Index Error', msg='Given index is not valid.'
+        matrixTools.matrixError(type,msg)
+    };
+
+    print(){
+        for(let row of this.matrix){ console.log(row); }
+    };
+
+    getRow(rowIndex){
+        matrixTools.rowExists(rowIndex,this.m,throwError=true)
+        return this.matrix[rowIndex];
+    };
+
+    getCol(colIndex){
+        matrixTools.colExists(colIndex,this.n,throwError=true);
+        return Array.from( this.matrix, (row) => row[colIndex] )
+    };
+
+    getEntry(arr){
+        if (!matrixTools.verifier('array',arr)){
+            arr = [...arr]
+        };
+        matrixTools.entryExists(rowInd=arr[0],colInd=arr[1],this.m,this.n,throwError=true);
+        return this.matrix[arr[0]][arr[1]];
+    };
+
+    setEntry(val,arr){
+        if (!matrixTools.verifier('array',arr)){
+            arr = [...arr]
+        };
+        if (matrixTools.entryExists(rowInd=arr[0],colInd=arr[1],this.m,this.n)){
+            this.matrix[rowIndex][colIndex] = val;
+        };
     };
 
 };
 
+module.exports = Matrix;
 
+if (require.main === module){
+    //  can perform tests here...
 
-export default Matrix;
+    const matrix = new Matrix(2,2);
+
+    console.log(matrix.print())
+
+};
--- Matrix/Tools.js
+++ Matrix/Tools.js
@@ -1,10 +1,10 @@
 /* -- Byimaan -- */
 
-export function matrixError(type='UnExcepted Error',msg="Got an error while executing.",header='Matrix Error'){
+function matrixError(type='UnExcepted Error',msg="Got an error while executing.",header='Matrix Error'){
     throw new Error(`${header}: ${type}, ${msg}`)
 };
 
-export function verifier(expectedType,value){
+function verifier(expectedType,value){
     switch (expectedType) {
         case 'integer': return Number.isInteger(value);
         case 'array': return Array.isArray(value);
@@ -13,10 +13,57 @@ export function verifier(expectedType,value){
     };
 };
 
-export function check(expectedType,...values){
+function check(expectedType,...values){
     try {
         return values.every(val => verifier(expectedType, val));
     } catch (unknownErr){
         matrixError('Invalid Input', 'Got Invalid inputs for comparing their types', 'Input Error');
     }
 };
+
+function isValidMatrix(m,n){
+    if (m == null || n == null){
+        matrixError('Dimension Error','The dimensions for matrix were not provided for initialization. Please pass the values for m and n in parameters.');
+    } else if (!check('integer',m,n)){
+        matrixError('Wrong Data Type', 'Dimension of matrix should be of integer data type.');
+    } else if ( m <= 0 || n <= 0 ){
+        matrixError('Invalid Input','The size of matrix should always be positive (m,n > 0)');
+    };
+};  
+
+function isValidFieldIndex(index, mOrN,throwError=false){
+    var type='Index Error', msg='Given index is not valid.'
+    if (index < mOrN && index >= 0) {
+        return true
+    } else {
+        if (throwError){ 
+            matrixError(type,msg) 
+        } else {return false};
+    }
+};
+
+function entryExists(rowInd,colInd,m,n,throwError=false){
+    return isValidFieldIndex(rowInd,m,throwError) && isValidFieldIndex(colInd,n,throwError);
+};
+
+const matrixTools = {
+    matrixError: matrixError,
+    verifier: verifier,
+    isValidMatrix: isValidMatrix,
+    check: check,
+    rowExists: isValidFieldIndex,
+    colExists: isValidFieldIndex,
+    entryExists: entryExists,
+}
+
+module.exports = { matrixTools };
+
+if (require.main === module) {
+    // Equivalent to Python's `if __name__ == "__main__"`
+
+    // testing 
+    var n1 = 3, n2 = 3, n3 = 7.0;
+
+    console.log(check('string',n1,n2,n3))
+
+}
